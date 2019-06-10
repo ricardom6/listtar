7d416d3ae2831718a95afb41e9d3c549bd504463
ricardom6 <ricardosilvademenezes@gmail.com>
2019-06-10 08:56:46 -0300

    futureData com problema

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
-
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -16,10 +16,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _toDoList = [];
+  final _toDoController = TextEditingController();
 
 
-
+  List _toDoList = [
+    "Tarefa A", "Tarefa B",
+    "Tarefa C", "Tarefa D",
+    "Tarefa E", "Tarefa F",
+  ];
+  void _addToDo(){
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["titulo"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["status"] = false;
+      newToDo["inicio"] = formatDate(DateTime.now(), [dd, '/', mm, '/', yyyy ]);
+      _toDoList.add(newToDo);
+    });
+  }
 
   //obter dados
   Future<String> _readData() async {
@@ -30,10 +44,6 @@ class _HomeState extends State<Home> {
       return null;
     }
   }
-
-
-
-
 
   //salvar dados
   Future<File> _saveData() async {
@@ -50,6 +60,71 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    //configurando o appBar
+    AppBar appBar = AppBar(
+      title: Text("Lista de Tarefas"),
+      backgroundColor: Colors.black26,
+      centerTitle: true,
+    );
+
+    TextField textField = TextField(
+      controller: _toDoController,
+      decoration: InputDecoration(
+        labelText: "Nova Tarefa",
+        labelStyle: TextStyle(color: Colors.black),
+      ),
+    );
+        RaisedButton btnAdd = RaisedButton(
+          onPressed: _addToDo,
+          color: Colors.black,
+          child: Text("ADD"),
+          textColor: Colors.white,
+    );
+
+    //pag 82
+    //configurando a linha do input e button
+    Row row = Row(
+      children: <Widget>[
+        Expanded(child: textField),
+        btnAdd,
+      ],
+    );
+
+    //configurando o container do topo
+    Container containerTop = Container(
+      padding: EdgeInsets.fromLTRB(18.0, 1.0, 6.0, 1.0),
+      child: row,
+    );
+    ListView listViewTarefas = ListView.builder(
+      padding: EdgeInsets.only(top:10.0),
+      itemCount: _toDoList.length,
+      itemBuilder: (context, index){
+        return CheckboxListTile(
+          title: Text(_toDoList[index]["titulo"]),
+          value: false,
+          secondary: CircleAvatar(
+            child: Icon(Icons.check),
+          ),
+        );
+      },
+    );
+
+
+
+    //configurando a coluna
+    Column column = Column(
+      children: <Widget> [
+        containerTop,
+        Expanded(          child: listViewTarefas,)
+      ],
+    );
+
+    //configurando o elemento na tela
+    Scaffold scaffold = Scaffold(
+      appBar: appBar,
+      backgroundColor: Colors.cyan,
+      body: column,
+    );
+    return scaffold;
   }
 }
